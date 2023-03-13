437f8a1e0eecd7f2516f126d85f46f692a0c1dfa
Akshay Jadhav <akshayjadhav2133@gmail.com>
2023-03-13 10:23:04 +0530

    bg updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-
-import './App.css'
-import Home from './Components/Home'
-import NavBar from './Components/NavBar'
-
+import "./App.css";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div className="min-w-[375px] bg-cover bg-center bg-no-repeat max-w-[1440px]  min-h-[100vh] flex justify-center items-start pt-16" style={{backgroundImage: `url('./bg-1.jpg')`}} >
-    <NavBar/>
-    <Home/>
-  </div>
-  )
+    <div
+      className="min-w-[375px] bg-cover bg-center bg-no-repeat max-w-[1440px]  min-h-[100vh] flex justify-center items-start pt-16"
+      style={{ backgroundImage: `url('./bg-3.jpg')` }}
+    >
+      <NavBar />
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
